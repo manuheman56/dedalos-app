41ab42b2471491f56e74fc7d52029665793f23c0
manuheman56 <manuhe.herrera.roman@gmail.com>
2025-12-22 18:17:44 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,7 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('dedalos-v1').then(cache => {
-      return cache.addAll(['index.html', 'manifest.json']);
-    })
-  );
+const CACHE = "dedalos-v2";
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["index.html", "manifest.json"])));
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });

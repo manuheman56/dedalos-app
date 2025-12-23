5a96f89b75d855bc529e1a6a08ec018c4926c6fc
manuheman56 <manuhe.herrera.roman@gmail.com>
2025-12-23 08:43:24 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,10 @@
-const CACHE = "dedalos-v2";
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["index.html", "manifest.json"])));
+const CACHE_NAME = "dedalos-v5";
+const FILES = ["index.html", "manifest.json"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
 });
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
